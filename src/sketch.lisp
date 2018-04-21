2585873881404f400f0864501b8a74cf2ffce69b
Lucas Vieira <lucasvieira@protonmail.com>
2018-04-21 18:44:00 -0300

    basic ship with desired input and some stars parallax

--- src/sketch.lisp
+++ src/sketch.lisp
@@ -1,9 +1,96 @@
 (ql:quickload 'game-sketch)
 
-(defun update (dt)
-  (declare (ignore dt)))
+(defmacro at-screen-center ()
+  `(list (ash 960 -1) (ash 540 -1)))
 
-(defun draw () )
+(defmacro from-center (position)
+  `(let ((center (at-screen-center)))
+     (list (+ (car ,position) (car center))
+	   (+ (cadr ,position) (cadr center)))))
+
+(defun gen-random-star ()
+  (list (random 960)
+	(random 540)))
+
+
+;;; Background-related
+(defparameter *stars-positions*
+  (loop for x from 0 to 2
+     collect (loop for y from 0 to 25 collect (gen-random-star))))
+(defparameter *stars-parallax-centers* '(0 0 0))
+(defparameter *stars-parallax-factors* '(0.5 0.8 1.1))
+
+(defun update-stars (dt)
+  (setf *stars-parallax-centers*
+	(loop for center in *stars-parallax-centers*
+	   for factor in *stars-parallax-factors*
+	   collect (mod (- center (* factor dt)) 960))))
+
+(defun draw-stars ()
+  (loop for layer in *stars-positions*
+     for n from 0 to 2
+     do (loop for star in layer
+	   do (let ((position (list (mod (+ (nth n *stars-parallax-centers*)
+					    (car star))
+					 960)
+				    (cadr star))))
+		(gl:with-pushed-matrix
+		  (gsk-util:transform-translate position)
+		  (gsk-util:no-stroke)
+		  (gsk-util:with-fill-color '(255 255 255)
+		    (gsk-util:ellipse '(0 0) '(5 5))
+		    ))))))
+
+;;; Ship-related
+
+(defparameter *ship-position* (from-center '(-400 0)))
+(defparameter *ship-rotation* 0)
+(defparameter *ship-max-speed* 1)
+
+(defun update-ship (dt)
+  ;; Rotate according to user input
+  (setf *ship-rotation*
+	(cond ((gsk-input:pressingp :up)
+	       (- *ship-rotation* (/ dt 500.0)))
+	      ((gsk-input:pressingp :down)
+	       (+ *ship-rotation* (/ dt 500.0)))
+	      ((<= (abs *ship-rotation*) 0.05)
+	       0.0)
+	      (t (+ *ship-rotation*
+		    (* (/ dt 375.0) (- (signum *ship-rotation*)))))))
+  ;; Clamp rotation between -quarterpi and quarterpi
+  (setf *ship-rotation* (gsk-util:clamp *ship-rotation*
+					:min (- (/ pi 4.0))
+					:max (/ pi 4.0)))
+  ;; Move around the screen according to rotation.
+  (let ((y (cadr *ship-position*)))
+    (when (not (= *ship-rotation* 0.0))
+      (setf (cadr *ship-position*)
+	    (+ y (* *ship-max-speed*
+		    (/ *ship-rotation* (/ pi 4.0))
+		    dt))))
+    ;; I was using a `mod` form on the previous statement
+    ;; to ensure a wraparound, but I figured I'd better do it
+    ;; manually since the ship was snapping weirdly
+    (when (< y 0) (setf (cadr *ship-position*) 540))
+    (when (> y 540) (setf (cadr *ship-position*) 0))))
+	       
+
+(defun draw-ship ()
+  (gl:with-pushed-matrix
+    (gsk-util:transform-translate *ship-position*)
+    (gsk-util:transform-rotate (+ *ship-rotation* (/ pi 2.0)))
+    (gsk-util:no-fill)
+    (gsk-util:with-stroke-color '(255 255 255)
+      (gsk-util:triangle '(0 -15) '(9 12) '(-9 12)))))
+
+(defun update (dt)
+  (update-stars dt)
+  (update-ship dt))
+
+(defun draw ()
+  (draw-stars)
+  (draw-ship))
 
 (gsk:add-update-callback 'update)
 (gsk:add-draw-callback 'draw)
